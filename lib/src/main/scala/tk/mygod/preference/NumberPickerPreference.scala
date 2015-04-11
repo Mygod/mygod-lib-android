3d9bc783098a7bf6e7101b4ddac0df47f1912071
Mygod <mygodstudio@gmail.com>
2015-04-11 18:21:16 +0800

    Various bugfixes for NumberPickerPreference

--- lib/src/main/scala/tk/mygod/preference/NumberPickerPreference.scala
+++ lib/src/main/scala/tk/mygod/preference/NumberPickerPreference.scala
@@ -10,45 +10,41 @@ import tk.mygod.R
 
 final class NumberPickerPreference(private val context: Context, attrs: AttributeSet = null)
   extends DialogPreference(context, attrs) with SummaryPreference {
-  private var picker: NumberPicker = _
-  private var min, max, value: Int = _
+  private val picker = new NumberPicker(context)
+  private var value: Int = _
 
   {
     val a: TypedArray = context.obtainStyledAttributes(attrs, R.styleable.NumberPickerPreference)
-    min = a.getInt(R.styleable.NumberPickerPreference_min, 0)
-    max = a.getInt(R.styleable.NumberPickerPreference_max, 0)
+    picker.setMinValue(a.getInt(R.styleable.NumberPickerPreference_min, 0))
+    picker.setMaxValue(a.getInt(R.styleable.NumberPickerPreference_max, Int.MaxValue))
     a.recycle
   }
   initSummary(context, attrs)
 
   def getValue = value
-  def getMin = min
-  def getMax = max
-  def setValue(n: Int) {
-    value = n
-    if (value < min) value = min
-    else if (value > max) value = max
+  def getMin = picker.getMinValue
+  def getMax = picker.getMaxValue
+  def setValue(i: Int) {
+    picker.setValue(i)
+    value = picker.getValue
     persistInt(value)
   }
-  def setMin(i: Int) {
-    min = if (i <= max) i else max
-    if (value < min) value = min
-  }
-  def setMax(i: Int) {
-    max = if (i >= min) i else min
-    if (value > max) value = max
-  }
+  def setMin(value: Int) = picker.setMinValue(value)
+  def setMax(value: Int) = picker.setMaxValue(value)
 
   override protected def onCreateDialogView = {
-    picker = new NumberPicker(context)
-    picker.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
-      ViewGroup.LayoutParams.WRAP_CONTENT))
-    picker.setMinValue(min)
-    picker.setMaxValue(max)
-    picker.setValue(value)
+    val parent = picker.getParent.asInstanceOf[ViewGroup]
+    if (parent != null) parent.removeView(picker)
     picker
   }
-  override protected def onDialogClosed(positiveResult: Boolean) = if (positiveResult) persistInt(picker.getValue)
+  override protected def onDialogClosed(positiveResult: Boolean) {
+    super.onDialogClosed(positiveResult)  // forward compatibility
+    if (!positiveResult) return
+    val i = picker.getValue
+    if (i == value || !callChangeListener(i)) return
+    setValue(i)
+    notifyChanged
+  }
   override protected def onGetDefaultValue(a: TypedArray, index: Int): AnyRef =
     a.getInt(index, getMin).asInstanceOf[AnyRef]
   override protected def onSetInitialValue(restorePersistedValue: Boolean, defaultValue: Any) =
