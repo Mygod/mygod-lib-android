221c30453f37052f57d994ca5bae9f3e60126b02
Mygod <mygodstudio@gmail.com>
2015-08-19 00:36:25 +0800

    Bugfix for API 16

--- lib/src/main/scala/tk/mygod/app/ActivityPlus.scala
+++ lib/src/main/scala/tk/mygod/app/ActivityPlus.scala
@@ -1,5 +1,6 @@
 package tk.mygod.app
 
+import android.os.Build
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.View
@@ -11,8 +12,16 @@ import scala.language.implicitConversions
  * @author Mygod
  */
 trait ActivityPlus extends AppCompatActivity with ContextPlus {
+  private var destroyed: Boolean = _
+
   def runOnUiThread(f: => Unit): Unit = runOnUiThread(new Runnable() { def run() = f })
 
   def makeSnackbar(text: CharSequence, duration: Int = Snackbar.LENGTH_LONG, view: View =
     getWindow.getDecorView.findViewById(android.R.id.content)) = Snackbar.make(view, text, duration)
+
+  override protected def onDestroy = {
+    super.onDestroy
+    destroyed = true
+  }
+  override def isDestroyed = if (Build.VERSION.SDK_INT >= 17) super.isDestroyed else destroyed
 }
