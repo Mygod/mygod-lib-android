62f35b811c24495f37228c163bc623dae7f04fba
Mygod <mygodstudio@gmail.com>
2015-10-05 16:27:55 +0800

    Improve MetricsUtils

--- lib/src/main/scala/tk/mygod/util/MetricsUtils.scala
+++ lib/src/main/scala/tk/mygod/util/MetricsUtils.scala
@@ -6,6 +6,6 @@ import android.content.Context
  * @author mygod
  */
 object MetricsUtils {
-  def dp2px(context: Context, dp: Int) = (dp * context.getResources.getDisplayMetrics.density).toInt
-  def sp2px(context: Context, sp: Int) = (sp * context.getResources.getDisplayMetrics.scaledDensity).toInt
+  def dp2px(context: Context, dp: Int) = (dp * context.getResources.getDisplayMetrics.density + .5).toInt
+  def sp2px(context: Context, sp: Int) = (sp * context.getResources.getDisplayMetrics.scaledDensity + .5).toInt
 }
