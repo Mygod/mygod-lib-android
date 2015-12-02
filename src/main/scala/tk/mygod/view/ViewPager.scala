594e578185cfff0830eacd42534a83044474936f
Mygod <mygodstudio@gmail.com>
2015-12-02 21:02:20 +0800

    Fix swiping in simple ViewPager

---
+++ src/main/scala/tk/mygod/view/ViewPager.scala
@@ -0,0 +1,37 @@
+package tk.mygod.view
+
+import android.content.Context
+import android.support.v4.view.{PagerAdapter, ViewPager => Base}
+import android.util.AttributeSet
+import android.view.{ViewGroup, MotionEvent, View}
+import android.view.ViewGroup.LayoutParams
+
+/**
+  * A useful ViewPager for Views.
+  *
+  * @author mygod
+  */
+class ViewPager(context: Context, attrs: AttributeSet) extends Base(context, attrs) {
+  private class Adapter extends PagerAdapter {
+    def isViewFromObject(view: View, obj: scala.Any) = view == obj.asInstanceOf[View]
+    def getCount = getChildCount
+
+    override def instantiateItem(collection: ViewGroup, position: Int) = collection.getChildAt(position)
+    override def destroyItem(collection: ViewGroup, position: Int, view: Any) =
+      collection.removeView(view.asInstanceOf[View])
+  }
+  private val adapter = new Adapter
+  setAdapter(adapter)
+
+  override def addView(child: View, index: Int, params: LayoutParams) {
+    super.addView(child, index, params)
+    adapter.notifyDataSetChanged
+  }
+  override def removeView(child: View) {
+    super.removeView(child)
+    adapter.notifyDataSetChanged
+  }
+
+  override def onInterceptTouchEvent(event: MotionEvent) = false
+  override def onTouchEvent(event: MotionEvent) = false
+}
