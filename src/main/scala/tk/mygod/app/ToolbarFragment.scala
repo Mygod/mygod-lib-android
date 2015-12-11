6b62b75c98d997d1defe7b9f9b2b4ab624c34f11
Mygod <mygodstudio@gmail.com>
2015-12-11 23:43:57 +0800

    Update ToolbarFragment

--- src/main/scala/tk/mygod/app/ToolbarFragment.scala
+++ src/main/scala/tk/mygod/app/ToolbarFragment.scala
@@ -1,5 +1,6 @@
 package tk.mygod.app
 
+import android.support.annotation.DrawableRes
 import android.support.v7.widget.Toolbar
 import android.view.{KeyEvent, View}
 import tk.mygod.TypedResource._
@@ -9,16 +10,21 @@ import tk.mygod.{R, TR}
 /**
  * @author Mygod
  */
+object ToolbarFragment {
+  final val BACK = R.drawable.abc_ic_ab_back_mtrl_am_alpha
+}
+
 trait ToolbarFragment extends StoppableFragment with SimpleKeyEventCallback {
   var toolbar: Toolbar = _
 
-  protected def configureToolbar(view: View, title: CharSequence, navigationIcon: Int = -1) {
+  protected def configureToolbar(view: View, title: CharSequence) {
     toolbar = view.findView(TR.toolbar)
     toolbar.setTitle(title)
-    if (navigationIcon != -1) {
-      toolbar.setNavigationIcon(if (navigationIcon == 0) R.drawable.abc_ic_ab_back_mtrl_am_alpha else navigationIcon)
-      toolbar.setNavigationOnClickListener(exit)
-    }
+  }
+
+  protected def setNavigationIcon(@DrawableRes navigationIcon: Int) = {
+    toolbar.setNavigationIcon(navigationIcon)
+    toolbar.setNavigationOnClickListener(exit)
   }
 
   override def onKeyUp(keyCode: Int, event: KeyEvent) = keyCode match {
