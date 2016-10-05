e2eebfd918f606c3ef8b21eba2bb13d9293d4695
Mygod <contact-git@mygod.be>
2016-10-05 23:23:37 +0800

    Use Math.round

--- src/main/scala/be/mygod/util/MetricsUtils.scala
+++ src/main/scala/be/mygod/util/MetricsUtils.scala
@@ -6,6 +6,6 @@ import android.content.Context
  * @author mygod
  */
 object MetricsUtils {
-  def dp2px(context: Context, dp: Int) = (dp * context.getResources.getDisplayMetrics.density + .5).toInt
-  def sp2px(context: Context, sp: Int) = (sp * context.getResources.getDisplayMetrics.scaledDensity + .5).toInt
+  def dp2px(context: Context, dp: Int) = Math.round(dp * context.getResources.getDisplayMetrics.density + .5)
+  def sp2px(context: Context, sp: Int) = Math.round(sp * context.getResources.getDisplayMetrics.scaledDensity + .5)
 }
