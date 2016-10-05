fa6eb827f3994423cb798cf6c9d05a27fac345eb
Mygod <contact-git@mygod.be>
2016-10-05 23:30:24 +0800

    Remove DropDownPreference (use the one in support lib instead)

--- src/main/scala/be/mygod/util/MetricsUtils.scala
+++ src/main/scala/be/mygod/util/MetricsUtils.scala
@@ -2,9 +2,6 @@ package be.mygod.util
 
 import android.content.Context
 
-/**
- * @author mygod
- */
 object MetricsUtils {
   def dp2px(context: Context, dp: Int) = Math.round(dp * context.getResources.getDisplayMetrics.density + .5)
   def sp2px(context: Context, sp: Int) = Math.round(sp * context.getResources.getDisplayMetrics.scaledDensity + .5)
