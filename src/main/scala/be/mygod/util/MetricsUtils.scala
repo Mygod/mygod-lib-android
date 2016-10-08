75a2baf79eda4e3222d389643a4073d5e5f6e752
Mygod <contact-git@mygod.be>
2016-10-08 17:36:16 +0800

    Bugfix

--- src/main/scala/be/mygod/util/MetricsUtils.scala
+++ src/main/scala/be/mygod/util/MetricsUtils.scala
@@ -3,6 +3,6 @@ package be.mygod.util
 import android.content.Context
 
 object MetricsUtils {
-  def dp2px(context: Context, dp: Int) = Math.round(dp * context.getResources.getDisplayMetrics.density + .5)
-  def sp2px(context: Context, sp: Int) = Math.round(sp * context.getResources.getDisplayMetrics.scaledDensity + .5)
+  def dp2px(context: Context, dp: Int) = Math.round(dp * context.getResources.getDisplayMetrics.density)
+  def sp2px(context: Context, sp: Int) = Math.round(sp * context.getResources.getDisplayMetrics.scaledDensity)
 }
