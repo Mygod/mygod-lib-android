81cba8a8f0616cf080e1dd189d107fb59e5a68c6
Mygod <contact-git@mygod.be>
2016-10-25 23:46:08 +0800

    Merge IOUtils from shadowsocks-android

--- src/main/scala/be/mygod/util/IOUtils.scala
+++ src/main/scala/be/mygod/util/IOUtils.scala
@@ -1,35 +1,54 @@
 package be.mygod.util
 
-import java.io.{OutputStreamWriter, InputStream, OutputStream}
-import java.util.Scanner
+import java.io._
+import java.nio.charset.Charset
+
 import be.mygod.util.CloseUtils._
 
 /**
  * @author Mygod
  */
 object IOUtils {
-  private val BUF_SIZE: Int = 0x1000
+  private val BUFFER_SIZE: Int = 0x8000
 
   def copy(from: InputStream, to: OutputStream): Long = {
-    val buf = new Array[Byte](BUF_SIZE)
-    var total = 0
-    var r = from.read(buf)
-    while (r >= 0) {
-      to.write(buf, 0, r)
-      total += r
-      r = from.read(buf)
+    val buf = new Array[Byte](BUFFER_SIZE)
+    var total = 0L
+    while (true) {
+      val count = from.read(buf)
+      if (count < 0) return total
+      to.write(buf, 0, count)
+      total += count
     }
     total
   }
 
-  def readAllText(stream: InputStream): String = {
-    val scanner = new Scanner(stream).useDelimiter("\\a")
-    if (scanner.hasNext) scanner.next else ""
-  }
-
-  def writeAllText(stream: OutputStream, text: String, charsetName: String = "UTF-8") =
-    autoClose(new OutputStreamWriter(stream, charsetName)) { writer =>
-      writer.write(text)
-      writer.flush()
+  def readAllText(in: InputStream): String = {
+    val builder = new StringBuilder()
+    val buffer = new Array[Byte](BUFFER_SIZE)
+    while (true) {
+      val count = in.read(buffer)
+      if (count >= 0) builder.append(new String(buffer, 0, count)) else return builder.toString()
     }
+    null
+  }
+  def readAllText(in: InputStream, charset: Charset): String = {
+    val builder = new StringBuilder()
+    val buffer = new Array[Byte](BUFFER_SIZE)
+    while (true) {
+      val count = in.read(buffer)
+      if (count >= 0) builder.append(new String(buffer, 0, count, charset)) else return builder.toString()
+    }
+    null
+  }
+  def readAllText(file: String): String = readAllText(new FileInputStream(file))
+  def readAllText(file: String, charset: Charset): String = readAllText(new FileInputStream(file), charset)
+
+  def writeAllText(stream: OutputStream, content: String) =
+    autoClose(new OutputStreamWriter(stream))(writer => writer.write(content))
+  def writeAllText(stream: OutputStream, content: String, charset: Charset) =
+    autoClose(new OutputStreamWriter(stream, charset))(writer => writer.write(content))
+  def writeAllText(file: String, text: String): Unit = writeAllText(new FileOutputStream(file), text)
+  def writeAllText(file: String, text: String, charset: Charset): Unit =
+    writeAllText(new FileOutputStream(file), text, charset)
 }
